d8f83f079cfaf1d7a2d678157c7e7cf732ca1443
Nov 2022 DevOps Class <120275123+becomeatechpro@users.noreply.github.com>
2023-01-22 13:57:49 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.17.164:9000/',
+  serverUrl: 'http://172.31.10.151:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsTechDom',
-	    //'sonar.login': '8cc5903f1a3b7ec268634aaf609048c2214e8e42',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.login': 'e0183537674bec68d7e128f8e216377608b39470',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
